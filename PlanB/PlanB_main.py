cdebd1b9386199b90ddf223150d84bfda5bbfb10
K-I-D620 <heto@tcd.ie>
2022-03-03 15:47:46 +0000

    Completed Plan B up till before stability in CF model

---
+++ PlanB/PlanB_main.py
@@ -0,0 +1,101 @@
+"""
+Counterfactual learning
+
+# debugging:
+ipython cf_learning/main.py
+
+"""
+
+from PlanB_model import CoPhyNet
+from torch import optim
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+# import argparse
+import ipdb
+import os
+from tqdm import *
+from random import choice
+import torch.nn.functional as F
+import time
+# from dataloaders.utils import *
+from causal_world.task_generators import generate_task
+from causal_world.envs.causalworld import CausalWorld
+from stable_baselines import PPO2
+import tensorflow as tf
+from stable_baselines.common.policies import MlpPolicy
+
+def Calc_num_objects(env_obs):
+    len_obs = env_obs.shape[0]
+    num_of_objects = int(len_obs/28) - 1
+    return num_of_objects
+
+def Convert_input_shape(stack_obs, timesteps, num_of_objects):
+    # 28 len vector for T,R1,R2,R3 in struct obs of CausalWorld and ...
+    # Another 28 for each object (object features, partial goal features) ...
+    # for struct obs
+    desired_input_obs = np.empty([timesteps, num_of_objects, 28 + 28])
+    lens_obs = stack_obs.shape[1]
+
+    for t in range(timesteps):
+        for k in range(num_of_objects):
+            # get T, R1, R2, R3 which is same for all objects
+            desired_input_obs[t, k, :28] = stack_obs[t, :28]
+            # Get features for each object
+            obj_index_lower = k * 28
+            obj_index_upper = obj_index_lower + 28
+            if obj_index_upper >= lens_obs:
+                desired_input_obs[t, k, 28:] = stack_obs[t, obj_index_lower:]
+            else:
+                desired_input_obs[t, k, 28:] = stack_obs[t, obj_index_lower:obj_index_upper]
+
+    return desired_input_obs
+
+
+def main():
+    # init gpu for training
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    kwargs_loader = {'batch_size': 32}
+    if device.type == 'cuda':
+        kwargs_loader.update({'num_workers': 10, 'pin_memory': True})
+
+    # init RL environment and model
+    task = generate_task(task_generator_id="pushing", 
+                            tool_block_mass=0.3)
+    env = CausalWorld(task=task, skip_frame = 1, enable_visualization=False)
+    RL_model = PPO2(MlpPolicy, env, verbose=1)
+
+    # Init and calc some values
+    obs = env.reset()
+    num_of_objects = Calc_num_objects(obs)
+    stack_input_obs = np.empty(28 + (num_of_objects * 28))
+    num_timesteps = 2
+
+    # Obtain sequence of observations to train CF model
+    for i in range(num_timesteps):
+        obs = env.reset()
+        if i == 0:
+            stack_input_obs = np.expand_dims(obs, axis=0)
+        elif i > 0:
+            stack_input_obs = np.concatenate((stack_input_obs, np.expand_dims(obs, axis=0)), axis=0)
+    # print("stack input obs: ", stack_input_obs)
+
+    # Get observations ab for CF model
+    desired_input_obs = Convert_input_shape(stack_input_obs, num_timesteps, num_of_objects)
+    tensor_input_obs_ab = torch.from_numpy(desired_input_obs)
+
+    # Get observations c for CF model
+    goal_intervention_dict = env.sample_new_goal()
+    success_signal, intervene_obs_c = env.do_intervention(goal_intervention_dict)
+    print("Goal Intervention success signal", success_signal)
+    intervene_obs_c = np.expand_dims(intervene_obs_c, axis=0)
+    desired_input_obs = Convert_input_shape(intervene_obs_c, 1, num_of_objects)
+    tensor_input_obs_c = torch.from_numpy(desired_input_obs)
+    #print("tensor input obs: ", tensor_input_obs_c.shape)
+
+    # Send to CF model
+    CF_model = CoPhyNet(num_objects=num_of_objects)
+    CF_model_output = CF_model.forward(tensor_input_obs_ab, tensor_input_obs_c)
+
+if __name__ == "__main__":
+    main()
---
+++ PlanB/PlanB_model.py
@@ -0,0 +1,222 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+#import torchvision.models as models # Caused Segmentation fault
+import ipdb
+
+class CoPhyNet(nn.Module):
+  def __init__(self, num_objects=5):
+    super().__init__()
+
+    self.K = num_objects
+
+    # AB
+    H = 32
+    # The 56 is linked to the last dim of the struct_obs_ab
+    self.mlp_inter = nn.Sequential(nn.Linear(2*(56),H),
+                                   nn.ReLU(),
+                                   nn.Linear(H,H),
+                                   nn.ReLU(),
+                                   nn.Linear(H,H),
+                                   nn.ReLU(),
+                                   )
+    D = H
+    self.D = D
+    # 56 from -1 dim of x and H is from -1 dim of E
+    self.mlp_out = nn.Sequential(nn.Linear(56+H, H),
+                                 nn.ReLU(),
+                                 nn.Linear(H, H))
+
+    # RNN
+    self.rnn = nn.GRU(D,H, num_layers=1, batch_first=True)
+
+    # Stability
+    self.mlp_inter_stab = nn.Sequential(nn.Linear(2*(H+3),H),
+                                        nn.ReLU(),
+                                        nn.Linear(H,H),
+                                        nn.ReLU(),
+                                        nn.Linear(H,H),
+                                        nn.ReLU(),
+                                        )
+    self.mlp_stab = nn.Sequential(nn.Linear(H+H+H+3, H),
+                                  nn.ReLU(),
+                                  nn.Linear(H, 1))
+
+    # Next position
+    self.mlp_inter_delta = nn.Sequential(nn.Linear(2*(H+56),H),
+                                         nn.ReLU(),
+                                         nn.Linear(H,H),
+                                         nn.ReLU(),
+                                         nn.Linear(H,H),
+                                         nn.ReLU(),
+                                         )
+    self.mlp_gcn_delta = nn.Sequential(nn.Linear(H*2 + 56, H),
+                                       nn.ReLU(),
+                                       nn.Linear(H, H))
+    self.rnn_delta = nn.GRU(H,H, num_layers=1, batch_first=True)
+    self.fc_delta = nn.Linear(H, 56)
+
+
+    # args
+    self.iterative_stab = True
+
+
+  def gcn_on_AB(self, struct_obs_ab):
+    list_out = []
+    K = struct_obs_ab.size(1) # Number of objects = n+m
+    T = struct_obs_ab.size(0) # Number of timesteps
+    for i in range(T):
+      x = struct_obs_ab[i,:,:56] # shape (n+m, 28 + 28)
+      # print("gcn_on_AB: x shape ", x.shape)
+
+      # compute interactions : e_t+1 = f(o_t^1,o_t^2,r)
+      x_1 = x.unsqueeze(0).repeat(K, 1, 1)  # (n+m, n+m, 56)
+      # print("gcn_on_AB: x_1 shape ", x_1.shape)
+      x_2 = x.unsqueeze(1).repeat(1, K, 1)  # (n+m, n+m, 56)
+      # print("gcn_on_AB: x_2 shape ", x_2.shape)
+      x_12 = torch.cat([x_1, x_2], -1).float() # (n+m, n+m, 2*(56))
+      E = self.mlp_inter(x_12) # E (1, n+m, 32)
+      # print("gcn_on_AB: E shape ", E.shape)
+
+      # next position : o_t+1^1 = f(o_t^1,e_t+1)
+      x_next_pos = x.unsqueeze(0) # x_next_pos (1, n+m, 56)
+      # print("gcn_on_AB x_next_pos shape: ", x_next_pos.shape)
+      out = self.mlp_out(torch.cat([x_next_pos, E], -1).float()) # cat([x_next_pos, E], -1) = (1, n+m, 56 + H)
+      # print("gcn_on_AB next pos out shape: ", out.shape) # out (1, n+m, H)
+      list_out.append(out) # list_out prob added a dim at 0 or 1
+
+    out = torch.stack(list_out, 1) # (B,T,K,H) in our case B=1
+
+    return out
+
+  def rnn_on_AB_up(self, seq_o, object_type=None):
+    if object_type is not None:
+      T = seq_o.shape[1] # Number of timesteps
+      object_type = object_type.unsqueeze(2).repeat(1,1,T,1)
+
+    K = seq_o.size(2) # Number of objects
+    list_out = []
+    for k in range(K):
+      x = seq_o[:,:,k] # same as seq_o[:,:,k,:]
+
+      if object_type is not None:
+        x = torch.cat([x, object_type[:,k]], -1)
+
+      out, _ = self.rnn(x) # (B,T,H)
+      list_out.append(out[:,-1]) # Only use latent rep of last layer
+
+    out = torch.stack(list_out, 1) # (B,K,H)
+    return out
+
+  # def pred_stab(self, confounders, pose_t, presence):
+  #   """
+  #   Given a timestep - predict the stability per object
+  #   :param confounders: (B,K,D)
+  #   :param pose_t: (B,K,3)
+  #   :param presence: (B,K)
+  #   :return: stab=(B,K,1)
+  #   """
+  #   list_stab = []
+  #   # x = input['pose_cd'][:,0] # (B,4,3)
+  #   x = pose_t # (B,4,3)
+  #   x = torch.cat([confounders, x], -1)
+  #   K = x.size(1)
+
+  #   # compute interactions : e_t+1 = f(o_t^1,o_t^2,r)
+  #   x_1 = x.unsqueeze(1).repeat(1, K, 1, 1)  # (B, K, K, D)
+  #   x_2 = x.unsqueeze(2).repeat(1, 1, K, 1)  # (B, K, K, D)
+  #   x_12 = torch.cat([x_1, x_2], -1)
+  #   E = self.mlp_inter_stab(x_12) # B,K,K,H
+  #   # E, E_sum = aggreg_E(E, input['presence_cd']) # B,K,H
+  #   E, E_sum = aggreg_E(E, presence) # B,K,H
+
+  #   # stability
+  #   stab = self.mlp_stab(torch.cat([x, E, E_sum], -1))
+
+  #   return stab # (B,K,1)
+
+  def pred_D(self, confounders, struct_obs_c, T=30):
+    """
+
+    :param stability: (B,K,1)
+    :param confounders: (1, n+m, H) a.k.a (B,K,H)
+    :param input: pose_cd= (1, n+m, 28+28) a.k.a (T,K,56)
+    :return: out=(B,10,K,3) stability=(B,10,4,1)
+    """
+    list_pose = []
+    list_stability = []
+    pose = struct_obs_c # (1, n+m, 28 + 28)
+    K = pose.size(1) # K = n+m
+    list_last_hidden = []
+    for i in range(T):
+      # Stability prediction
+      # if i == 0 or self.iterative_stab == 'true':
+      #   stability = self.pred_stab(confounders, pose, presence_c)
+      # list_stability.append(stability)
+
+      # Cat
+      x = torch.cat([pose, confounders], -1) #.detach() ??? # x (1, n+m, 56 + H)
+      # print("pred_D: x shape ", x.shape)
+
+      # compute interactions : e_t+1 = f(o_t^1,o_t^2,r)
+      x_1 = x.unsqueeze(1).repeat(1, K, 1, 1)  # (1, n+m, n+m, 56+H)
+      x_2 = x.unsqueeze(2).repeat(1, 1, K, 1)  # (1, n+m, n+m, 56+H)
+      x_12 = torch.cat([x_1, x_2], -1).float() # (1, n+m, n+m, 2*(56+H))
+      E = self.mlp_inter_delta(x_12) # (1, n+m, n+m, H)
+      # print("pred_D: E shape ", E.shape)
+
+      # next position : o_t+1^1 = f(o_t^1,e_t+1) with RNN on top
+      _in = self.mlp_gcn_delta(torch.cat([x_1, E], -1).float()) # cat([x_1, E], -1)= shape(1, n+m, n+m, 2*(H)+56)
+      # _in (1, 1, n+m, H)
+      # print("pred_D: _in shape ", _in.shape)
+      B = _in.size(0) # Breadth of network = 1
+      list_new_hidden = []
+      for k in range(K):
+        if i == 0:
+          hidden, *_ = self.rnn_delta(_in[:,:,k]) # _in shape (B,1,H)
+          # hidden (B, 1, H)
+          # print("pred_D: hidden shape ", hidden.shape)
+        else:
+          hidden, *_ = self.rnn_delta(_in[:,:,k], list_last_hidden[k]) # (B,1,H)
+          # hidden (B, 1, H)
+          # print("pred_D: hidden shape ", hidden.shape)
+        list_new_hidden.append(hidden)
+      list_last_hidden = list_new_hidden
+      hidden = torch.cat(list_last_hidden, 1) # (B,K,H)
+
+      delta = self.fc_delta(hidden) # delta (B,K,56)
+
+      # if self.training:
+      #   alpha = 0.01
+      #   delta = delta * (1 - torch.sigmoid(stability/alpha)) # .detach() ???
+      # else:
+      #   delta = delta * (1-(stability > 0).float())
+      pose = pose + delta
+
+      list_pose.append(pose)
+
+    pose = torch.stack(list_pose, 1) # (B,T,K,3)
+    # stability = torch.stack(list_stability, 1) # (B,T,K,3)
+
+    return pose #, stability
+
+  def forward(self, struct_obs_ab, struct_obs_c):
+
+    T = struct_obs_ab.size(0) - 1
+
+    # Run a GCN on AB
+    seq_o = self.gcn_on_AB(struct_obs_ab) 
+    print("forward gcn on ab seq_o shape: ", seq_o.shape)
+
+    # Run a RNN on the outputs of GCN
+    confounders = self.rnn_on_AB_up(seq_o) # (B,K,H)
+    print("forward rnn on gcn confounders shape: ", confounders.shape)
+
+    # pred
+    out = self.pred_D(confounders, struct_obs_c, T=T)
+    print("forward out shape: ", out.shape)
+    # # stability = (B,T-1,K,1)
+    # stability = stability.squeeze(-1)
+    # print("forward stability shape: ", stability.shape)
+
+    return out
